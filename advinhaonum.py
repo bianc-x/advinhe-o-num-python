0fcb67868a3cd62a7e656f1ff0949bdc35391090
Bianca <144060187+bianc-x@users.noreply.github.com>
2024-04-06 22:28:22 -0300

    Add files via upload

---
+++ advinhaonum.py
@@ -0,0 +1,13 @@
+import random
+
+def adv(x):
+    random_number = random.randint(1, x)
+    adv = 0
+    while adv != random_number:
+        adv = int(input(f'Chute um numero entre 1 e {x}: '))
+        if adv < random_number:
+            print('Tente novamente, o numero eh maior!')
+        elif adv > random_number:
+            print('Tente novamente, o numero eh menor!')
+
+    print(f'Parabens! Voce acertou, o numero eh {random_number}!')
